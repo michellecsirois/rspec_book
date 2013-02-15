6322d8efe2b3dee7ebeb47b11eaf7f1759382bfc
Michelle Sirois <michelle.c.sirois@gmail.com>
2013-02-15 15:40:33 -0700

    Completed Part I

---
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -0,0 +1,31 @@
+class Output
+	def messages
+		@messages ||= []
+	end
+
+	def puts(message)
+		messages << message
+	end
+
+	def output
+		@output ||= Output.new
+	end
+end
+
+	Given /^the secret code is "([^"]*)"$/ do |secret|
+		@game = Codebreaker::Game.new(output)
+		@game.start(secret)
+	end
+
+	When /^I start a new game$/ do
+		game = Codebreaker::Game.new(output)
+		game.start('1234')
+	end
+
+	When /^I guess "([^"]*)"$/ do |guess|
+		@guess.guess(guess)
+	end
+
+	Then /^the mark should be "([^"]*)"$/ do |mark|
+		output.messages.should include(mark)
+	end
